84d0196322a636f8d33cc9c3d66077181303b349
Rhapsodia-hash <133169288+Rhapsodia-hash@users.noreply.github.com>
2024-02-16 16:54:55 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -334,7 +334,7 @@ async def members_command(self, ctx):
 @app.route("/")
 def home():
     client.run("MTIwMDE0OTM2NjIwNDI4MDkxMg.GHEL3Z._7BaqMwqz3AGCOfoNSWzdSWQRTvQCs9gFM10SM")
-    return "Estamos con el cliente activo"
+    return "Estamos dentro"
 
 
 @app.route("/about")
